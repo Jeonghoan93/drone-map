1184ad6469995302f753708cf390336eb7ef01aa
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-08-22 15:30:07 +0200

    fixed unit test for api based on code

--- src/services/api.spec.ts
+++ src/services/api.spec.ts
@@ -1,6 +1,6 @@
-import * as api from "./api";
-import { Drone, Boundary } from "src/shared/types";
 import fetchMock from "fetch-mock";
+import { Boundary, Drone } from "src/shared/types";
+import * as api from "./api";
 
 beforeEach(() => {
   fetchMock.restore();
@@ -74,11 +74,29 @@ describe("API methods", () => {
       heightMeters: 110,
     };
 
+    const mockExistingDrones: Drone[] = [
+      {
+        id: 1,
+        name: "Drone1",
+        description: "Sample drone",
+        position: { lat: 40, lon: 50 },
+        speedMs: 5,
+        heading: 60,
+        heightMeters: 100,
+      },
+    ];
+    fetchMock.mock(
+      `${api.BASE_URL}/drones`,
+      JSON.stringify(mockExistingDrones),
+      {
+        method: "GET",
+      }
+    );
+
     const mockResponse: Drone = {
       id: 2,
       ...newDroneData,
     };
-
     fetchMock.mock(`${api.BASE_URL}/drones`, JSON.stringify(mockResponse), {
       method: "POST",
     });
